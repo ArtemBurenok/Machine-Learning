a0467d135b2f28ac3282f3ca3238be821861bc2c
ArtemBurenok <71509633+ArtemBurenok@users.noreply.github.com>
2021-03-14 18:56:50 +0300

    mnist

--- machine learning/Boston House.py
+++ machine learning/Boston House.py
@@ -4,11 +4,12 @@ from sklearn.model_selection import cross_val_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 import pandas as pd
+from sklearn.linear_model import LinearRegression
 
 cancer = load_breast_cancer()
 data = pd.DataFrame(cancer.data, columns=cancer.feature_names)
 target = pd.DataFrame(cancer.target, columns=["target"])
-
+"""
 scaler = StandardScaler()
 scaler.fit(data)
 X_scaled = scaler.transform(data)
@@ -16,6 +17,11 @@ X_scaled = scaler.transform(data)
 pca = PCA(n_components=2)
 pca.fit(X_scaled)
 X_pca = pca.transform(X_scaled)
+"""
+X_train, X_test, y_train, y_test = train_test_split(data, target, random_state=42, test_size=0.2)
 
+model = LinearRegression()
+model.fit(X_train, y_train)
 
-score = cross_val_score()
+print(model.score(X_train, y_train))
+print(model.score(X_test, y_test))
